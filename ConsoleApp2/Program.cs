3ad39f13f67d9d6644198ff15d1b78f2bde311cd
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-05 11:19:21 +0400

    new porojekt

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin = "bxsam yagis basladi";
+            for (int i = 0; i < metin.Length; i++)
+                
+
+            {
+                if (metin[i] == 'a')
+                    Console.WriteLine("metinde a herifi var");
+                else
+                {
+                    Console.WriteLine("metinde a herifi yoxdur");  
+                }
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
